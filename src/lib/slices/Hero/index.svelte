28a507a1c7a3958f588185a93e829886c6d076c3
Mel <jones.mel@gmail.com>
2024-07-08 22:44:53 +1000

    style: add grid

--- src/lib/slices/Hero/index.svelte
+++ src/lib/slices/Hero/index.svelte
@@ -10,20 +10,29 @@
 	class="px-4 md:px-6"
 >
 	<div class="mx-auto w-full max-w-7xl">
-		<h1
-			class="mb-2 md:mb-8 text-[clamp(3rem,20vmin,13rem)] font-extrabold leading-none tracking-tighter text-nowrap"
-		>
-			<span class="block text-slate-300">
-				{slice.primary.firstname}
-			</span>
-			<span class="block text-slate-500 -mt-[.2em]">
-				{slice.primary.last_name}
-			</span>
-		</h1>
-		<span
-			class="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-transparent uppercase text-2xl font-bold tracking-[.2em] md:text-4xl"
-		>
-			{slice.primary.tag_line}
-		</span>
+		<div class="grid min-h-[65vh] grid-cols-1 items-center md:grid-cols-2">
+			<div
+				class="text-slate-300 relative z-10 row-span-1 row-start-1 -my-10 aspect-[1/1.3] overflow-hidden md:col-span-1 md:col-start-2"
+			>
+				Shape Scene
+			</div>
+			<div class="col-start-1 md:col-start-1">
+				<h1
+					class="mb-2 md:mb-8 text-[clamp(3rem,20vmin,13rem)] font-extrabold leading-none tracking-tighter text-nowrap"
+				>
+					<span class="block text-slate-300">
+						{slice.primary.firstname}
+					</span>
+					<span class="block text-slate-500 -mt-[.2em]">
+						{slice.primary.last_name}
+					</span>
+				</h1>
+				<span
+					class="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-transparent uppercase text-2xl font-bold tracking-[.2em] md:text-4xl"
+				>
+					{slice.primary.tag_line}
+				</span>
+			</div>
+		</div>
 	</div>
 </section>
