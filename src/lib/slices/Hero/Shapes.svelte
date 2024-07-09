243586c8d92802570e3f60030cb34da3f538a243
Mel <jones.mel@gmail.com>
2024-07-09 19:50:48 +1000

    feat: add threlte and setup hero canvas

---
+++ src/lib/slices/Hero/Shapes.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { T as Threlte } from '@threlte/core';
+	import { PerspectiveCamera } from 'three';
+	import * as THREE from 'three';
+</script>
+
+<Threlte.PerspectiveCamera
+	makeDefault
+	position={[0, 0, 25]}
+	aspect={1}
+	fov={30}
+	near={1}
+	far={40}
+/>
