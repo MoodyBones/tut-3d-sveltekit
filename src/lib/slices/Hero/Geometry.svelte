3bbd89fc82de5aadeceba412fb7a070291631473
Mel <jones.mel@gmail.com>
2024-07-09 22:08:48 +1000

    feat: add get random material function

--- src/lib/slices/Hero/Geometry.svelte
+++ src/lib/slices/Hero/Geometry.svelte
@@ -2,16 +2,29 @@
 	import { T as Threlte } from '@threlte/core';
 	import { Float } from '@threlte/extras';
 	import * as THREE from 'three';
+	import { gsap } from 'gsap';
 
 	export let position: [number, number, number] = [0, 0, 0];
 	export let geometry: THREE.BufferGeometry = new THREE.IcosahedronGeometry(3);
 	export let rate = 0.5;
 
-	const material = new THREE.MeshStandardMaterial({
-		color: 'cornflowerblue',
-		roughness: 0.1,
-		metalness: 0.5
-	});
+	const material = new THREE.MeshStandardMaterial();
+
+	const materialParams = [
+		{
+			color: 'cornflowerblue',
+			roughness: 0.1,
+			metalness: 0.5
+		},
+		{
+			color: 'purple',
+			roughness: 0.8
+		}
+	];
+
+	function getRandomMaterial() {
+		return new THREE.MeshStandardMaterial(gsap.utils.random(materialParams));
+	}
 </script>
 
 <Threlte.Group position={position.map((p) => p * 2)}>
@@ -21,6 +34,6 @@
 		rotationIntensity={5 * rate}
 		floatIntensity={5 * rate}
 	>
-		<Threlte.Mesh {geometry} {material}></Threlte.Mesh>
+		<Threlte.Mesh {geometry} material={getRandomMaterial()}></Threlte.Mesh>
 	</Float>
 </Threlte.Group>
